09130721dab3244cd412875b6346b54baeec79ab
Petr Kočí <petr.koci@samizdat.cz>
2019-07-17 19:49:56 +0200

    schema

--- js/parseData.js
+++ js/parseData.js
@@ -4,10 +4,92 @@ const schema = {
     'Countries::Country name option 1 E' : {
         prop : 'stat',
         type : String 
-    }
+    },
+    'Year of Census in NSO' : {
+        prop : 'rokScitani',
+        type : Number
+    },
+    'PreCubsFE' : {
+        prop : 'benjaminciHolky',
+        type : Number
+    },
+    'PreCubsMA' : {
+        prop : 'benjaminciKluci',
+        type : Number
+    },
+    'CubsFE' : {
+        prop : 'svetlusky',
+        type : Number
+    },
+    'CubsMA' : {
+        prop : 'vlcata',
+        type : Number
+    },
+    'ScoutsFE' : {
+        prop : 'skautky',
+        type : Number
+    },
+    'ScoutsMA' : {
+        prop : 'skauti',
+        type : Number
+    },
+    'SeniorFE' : {
+        prop : 'starsiSkautky',
+        type : Number
+    },
+    'SeniorMA' : {
+        prop : 'starsiSkauti',
+        type : Number
+    },
+    'RoversFE' : {
+        prop : 'roveriHolky',
+        type : Number
+    },
+    'RoversMA' : {
+        prop : 'roveriKluci',
+        type : Number
+    },
+    'VolunteerFE' : {
+        prop : 'dobrovolnice',
+        type : Number
+    },
+    'VolunteerMA' : {
+        prop : 'dobrovolnici',
+        type : Number
+    },
+    'ProfFE' : {
+        prop : 'zamestnankyne',
+        type : Number
+    },
+    'ProfMA' : {
+        prop : 'zamestnanci',
+        type : Number
+    },
+    'OtherFE' : {
+        prop : 'jine',
+        type : Number
+    },
+    'OtherMA' : {
+        prop : 'jini',
+        type : Number
+    },
+    'TOTWOSM' : {
+        prop : 'celkem',
+        type : Number
+    },
+    'Previous Year of Census' : {
+        prop : 'rokMinulehoScitani',
+        type : Number
+    },
+    'Previous Year of Census' : {
+        prop : 'rokMinulehoScitani',
+        type : Number
+    },
+    'PREVTOTWOSM' : {
+        prop : 'celkemMinule',
+        type : Number
+    },    
 };
-
-console.log( {schema} );
 
 readXlsxFile('../data/data.xlsx', { schema }).then((rows) => {
     console.log(rows);
