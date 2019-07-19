e26f8c56423b6ccfc3f8f28bc0addf8891c7432f
Petr Kočí <petr.koci@samizdat.cz>
2019-07-19 14:40:02 +0200

    sjednocené názvy zemí

--- js/parseData.js
+++ js/parseData.js
@@ -6,6 +6,10 @@ const schema = {
         prop : 'stat',
         type : String 
     },
+    'Countries::WOSM region E' : {
+        prop : 'region',
+        type : String 
+    },
     'Year of Census in NSO' : {
         prop : 'rokScitani',
         type : Number
@@ -92,7 +96,44 @@ const schema = {
     },    
 };
 
+const jeStatvgeoJSON = staty => {
+    const mapa = JSON.parse(fs.readFileSync('../node_modules/@highcharts/map-collection/custom/world-palestine.geo.json'));
+    const nazvyStatuvMape = [];
+    mapa.features.forEach(stat => nazvyStatuvMape.push(stat.properties.name));
+    staty.forEach(i => {
+        if (!nazvyStatuvMape.includes(i.stat)) {console.log(i.stat)};
+    });
+}
+
+const sjednotStaty = staty => {
+    staty.forEach(i => {
+        if (i.stat === 'Aruba') {i.stat = 'Netherlands'};
+        if (i.stat === 'Bahamas') {i.stat = 'The Bahamas'};
+        if (i.stat === 'Plurinational State of Bolivia') {i.stat = 'Bolivia'};
+        if (i.stat === 'Brunei Darussalam') {i.stat = 'Brunei'};
+        if (i.stat === 'Cabo Verde') {i.stat = 'Cape Verde'};
+        if (i.stat === 'Democratic Republic of The Congo') {i.stat = 'Democratic Republic of the Congo'};
+        if (i.stat === 'Côte d’Ivoire') {i.stat = 'Ivory Coast'};
+        if (i.stat === 'Curaçao') {i.stat = 'Netherlands'};
+        if (i.stat === 'Guinea-Bissau') {i.stat = 'Guinea Bissau'};
+        if (i.stat === 'Hong Kong') {i.stat = 'China'};
+        if (i.stat === 'Republic of Korea') {i.stat = 'South Korea'};
+        if (i.stat === 'Macao') {i.stat = 'China'};
+        if (i.stat === 'The former Yugoslav Republic of Macedonia') {i.stat = 'Macedonia'};
+        if (i.stat === 'Republic of Moldova') {i.stat = 'Moldova'};
+        if (i.stat === 'State of Palestine') {i.stat = 'West Bank'};
+        if (i.stat === 'Russian Federation') {i.stat = 'Russia'};
+        if (i.stat === 'Sao Tomé and Principe') {i.stat = 'Sao Tome and Principe'};
+        if (i.stat === 'Serbia') {i.stat = 'Republic of Serbia'};
+        if (i.stat === 'Syrian Arab Republic') {i.stat = 'Syria'};
+        if (i.stat === 'Timor-Leste') {i.stat = 'East Timor'};
+        if (i.stat === 'Bolivarian Republic of Venezuela') {i.stat = 'Venezuela'};        
+    })        
+}
+
 readXlsxFile('../data/data.xlsx', { schema }).then((rows , errors) => {
     console.log(errors);
+    sjednotStaty(rows.rows);
+    jeStatvgeoJSON(rows.rows);
     fs.writeFileSync('../data/data.json', JSON.stringify(rows.rows));
 });
--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 ﻿import "./byeie"; // loučíme se s IE
-import Highmaps from 'highcharts/highmaps';
+import Highcharts from 'highcharts/highmaps';
+import map from '@highcharts/map-collection/custom/world-palestine.geo.json';
 
 fetch('https://data.irozhlas.cz/jamboree/data/data.json')
   .then(response => response.json()) // nebo .text(), když to není json
   .then(data => {
-    console.log(data);
+    console.log(Highcharts, map);
   });
